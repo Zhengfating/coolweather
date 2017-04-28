b6b5fc43a756b6a21961f9d7564f27223dae4425
Alfx <1162401790@qq.com>
2017-04-28 17:18:36 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,18 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by dellpc on 2017/4/28.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+}
---
+++ app/src/main/java/com/coolweather/android/gson/Now.java
@@ -0,0 +1,23 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by dellpc on 2017/4/28.
+ */
+
+public class Now {
+
+    @SerializedName("tmp")
+    public String temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class More{
+
+        @SerializedName("txt")
+        public String info;
+
+    }
+}
